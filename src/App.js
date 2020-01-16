b53ddfbbcd8d5a76562f9408aaf0b7b28811322f
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-01-16 02:25:42 -0300

    Integrando com a API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import api from './services/api';
+
 function App() {
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+
+      },
+      {
+        timeout: 30000
+      });
+
+    getDevs();
+  }, []);
+
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    getDevs();
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
+  async function getDevs() {
+    const { data } = await api.get('/devs');
+    setDevs(data);
+  }
+
+  function renderDev() {
+    return devs.map(({ name, bio, avatar_url, techs, github_username }) => {
+      techs = techs.join(', ');
+      return (
+        <li className="dev-item" key={github_username}>
+          <header>
+            <img src={avatar_url} alt="Mateus Henrique" />
+            <div className="user-info">
+              <strong>{name}</strong>
+              <span>{techs}</span>
+            </div>
+          </header>
+          <p>{bio}</p>
+          <a href={`https://github.com/${github_username}`}>Acessar perfil no github</a>
+        </li>
+      )
+    })
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
 
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="username_github" required />
+            <input name="github_username" id="username_github" required value={github_username} onChange={e => setGithubUsername(e.target.value)} />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input name="techs" id="techs" required value={techs} onChange={e => setTechs(e.target.value)} />
           </div>
 
           <div className="input-group">
 
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input type="number" name="latitude" id="latitude" required value={latitude} onChange={e => setLatitude(e.target.value)} />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input type="number" name="longitude" id="longitude" required value={longitude} onChange={e => setLatitude(e.target.value)} />
             </div>
 
           </div>
@@ -42,50 +111,7 @@ function App() {
       <main>
 
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/34848657?s=460&v=4" alt="Mateus Henrique" />
-              <div className="user-info">
-                <strong>Mateus Henrique</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação, desenvolvedor em um projeto da Polícia Civil e simpatizante de programação orientada a ilusões</p>
-            <a href="https://github.com/mateuschaves">Acessar peril no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/34848657?s=460&v=4" alt="Mateus Henrique" />
-              <div className="user-info">
-                <strong>Mateus Henrique</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação, desenvolvedor em um projeto da Polícia Civil e simpatizante de programação orientada a ilusões</p>
-            <a href="https://github.com/mateuschaves">Acessar peril no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/34848657?s=460&v=4" alt="Mateus Henrique" />
-              <div className="user-info">
-                <strong>Mateus Henrique</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação, desenvolvedor em um projeto da Polícia Civil e simpatizante de programação orientada a ilusões</p>
-            <a href="https://github.com/mateuschaves">Acessar peril no github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/34848657?s=460&v=4" alt="Mateus Henrique" />
-              <div className="user-info">
-                <strong>Mateus Henrique</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Estudante de Sistemas de Informação, desenvolvedor em um projeto da Polícia Civil e simpatizante de programação orientada a ilusões</p>
-            <a href="https://github.com/mateuschaves">Acessar peril no github</a>
-          </li>
+          {renderDev()}
         </ul>
       </main>
     </div>
